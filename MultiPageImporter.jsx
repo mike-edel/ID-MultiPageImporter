32f9fad21e638be20991b91aee37b0152463d3be
Mike <mike-edel@users.noreply.github.com>
2022-08-30 22:30:52 +0200

    Revert "Merge pull request #28 from noamsmadja/patch-1"
    This reverts commit b5673a2926d6750b60fa0b0e15c70c88d6d53448, reversing
    changes made to 433fda4a00454d1aa9f2035a46851c9bee30546e.

--- MultiPageImporter.jsx
+++ MultiPageImporter.jsx
@@ -1,4 +1,4 @@
-// MultiPageImporter2.6.2 jsx
+﻿// MultiPageImporter2.6.2 jsx
 // An InDesign CS4 JavaScript
 // 28 MAR 2010
 // Copyright (C) 2008-2009 Scott Zanelli. lonelytreesw@gmail.com
@@ -64,7 +64,6 @@ var percX = 100;
 var percY = 100;
 var mapPages = 0;
 var reverseOrder = 0;
-var skipVar = 1;
 var rotate = 0;
 var positionType = 4; // 4 = center
 
@@ -252,7 +251,6 @@ if(dLog.show() == 1)
 	docStartPG = Number(dLog.docStartPG.text);
 	cropType = dLog.cropType.selection.index;
 	offsetX = Number(dLog.offsetX.text);
-	skipVar = Number(dLog.skipVar.text);
 	offsetY = Number(dLog.offsetY.text);
 	percX = Number(dLog.percX.text);
 	percY = Number(dLog.percY.text);
@@ -486,11 +484,11 @@ else if(reverseOrder && noPDFError)
 }
 else
 {
-	addPages(docStartPG, startPG, endPG, skipVar);
+	addPages(docStartPG, startPG, endPG);
 }
 
 // Kill the Object style
-//tempObjStyle.remove();
+tempObjStyle.remove();
 
 // Save prefs and then restore original app/doc settings
 savePrefs(false);
@@ -500,14 +498,14 @@ restoreDefaults(true);
 exit();
 
 // Place the requested pages in the document
-function addPages(docStartPG, startPG, endPG, skipVar)
+function addPages(docStartPG, startPG, endPG)
 {
 	var currentPDFPg = 0;
 	var firstTime = true;
 	var addedAPage = false;
 	var zeroBasedDocPgCnt = docPgCount - 1;
 
-	for(i = docStartPG - 1, currentInputDocPg = startPG; currentInputDocPg <= endPG; currentInputDocPg++, i+=skipVar)
+	for(i = docStartPG - 1, currentInputDocPg = startPG; currentInputDocPg <= endPG; currentInputDocPg++, i++)
 	{
 
 		if(placementINFO.kind == PDF_DOC)
@@ -701,9 +699,7 @@ function makeDialog()
 	dLog.pan1.add('statictext',  [10,94,190,109], "Start Placing on Doc Page:"); 
 	dLog.docStartPG = dLog.pan1.add('edittext', [10,114,70,137], "1");
 	dLog.docStartPG.onChange = docStartPGValidator;
-	dLog.pan1.add('statictext',  [75,112,190,137], "skip every "); 
-	dLog.skipVar = dLog.pan1.add('edittext', [140,114,180,137], "1");
-
+	
 	/***********************/
 	/* Lower Left Panel */
 	/***********************/
